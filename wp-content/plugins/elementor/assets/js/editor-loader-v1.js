ca85a84d6306e77aec6b204e1861469d0d05af6c
Fabio Biocchetti <24655386+fabiocchetti@users.noreply.github.com>
2025-08-20 00:59:32 +0200

    Update hardcoded URLs

--- wp-content/plugins/elementor/assets/js/editor-loader-v1.js
+++ wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -1,6 +1,12 @@
 /*! elementor - v3.31.0 - 11-08-2025 */
-(()=>{"use strict";
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /*!*******************************************************!*\
   !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
   \*******************************************************/
-window.elementor.start();})();
+
+
+window.elementor.start();
+/******/ })()
+;
+//# sourceMappingURL=editor-loader-v1.js.map
